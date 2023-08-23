80e335fbb2f7cfc87aa9ce8ec703a20cefe87ae3
marina <tm20388@gmail.com>
2023-08-23 14:54:59 +0600

    Загрузить классы в модуле HalykEpay

--- lib/halyk_epay.rb
+++ lib/halyk_epay.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'halyk_epay/token'
+require 'halyk_epay/payment'
+require 'halyk_epay/transaction'
 
 module HalykEpay
   class BadRequestError < StandardError; end
