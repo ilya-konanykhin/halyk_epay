0ccf6a436019e9664d4d98639b2321bb5acb51de
marina <tm20388@gmail.com>
2023-08-23 14:45:04 +0600

    Добавить класс Transaction к модулю HalykEpay

---
+++ lib/halyk_epay/transaction.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module HalykbankEpay
+  class Transaction
+    SUCCESS_AMOUNT_STATUS = 'CHARGE'
+    BLOCK_AMOUNT_STATUS = 'AUTH'
+    DEBITED_AMOUNT_STATUS = 'CHARGE'
+    INITIAL_TRANSACTION_STATUS = 'NEW'
+    RETRY_REQUEST_STATUS = 107
+    FAILED_TRANSACTION_STATUS = %w(REJECT 3D FAILED CANCEL_OLD CANCEL)
+
+    attr_accessor :data
+
+    def initialize(data)
+      @data = data
+    end
+
+    def status
+      data['transaction']['statusName']
+    end
+
+    def success?
+      data['transaction'] && data['transaction']['statusName'] == SUCCESS_AMOUNT_STATUS
+    end
+
+    def failed?
+      data['transaction'] && FAILED_TRANSACTION_STATUS.include?(data['transaction']['statusName'])
+    end
+
+    def retry_late?
+      data['resultCode'] == RETRY_REQUEST_STATUS || (data['transaction'] && data['transaction']['statusName'] == INITIAL_TRANSACTION_STATUS)
+    end
+
+    def can_confirm?
+      can_update?
+    end
+
+    def can_cancel?
+      can_update?
+    end
+
+    def can_refund?
+      data['transaction'] && data['transaction']['statusName'] == DEBITED_AMOUNT_STATUS
+    end
+
+    private
+
+    def can_update?
+      data['transaction'] && data['transaction']['statusName'] == BLOCK_AMOUNT_STATUS
+    end
+  end
+end
+
+
